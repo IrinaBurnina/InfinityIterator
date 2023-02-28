bed591699401176773e274a09ea3cd2c0f864110
Irina Burnina <irinkaburninka@gmail.com>
2023-03-01 00:06:58 +0300

    add iterator method to return

--- src/main/java/Randoms.java
+++ src/main/java/Randoms.java
@@ -20,17 +20,7 @@ public class Randoms implements Iterable<Integer> {
 
     @Override
     public Iterator<Integer> iterator() {
-        return null;
-    }
-
-    @Override
-    public void forEach(Consumer<? super Integer> action) {
-        Iterable.super.forEach(action);
-    }
-
-    @Override
-    public Spliterator<Integer> spliterator() {
-        return Iterable.super.spliterator();
+        return set.iterator();
     }
 
 }
